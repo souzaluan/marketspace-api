35df656c6a0d4dada45237b6205b2e8f2c113306
souzaluan <luan.souza69@hotmail.com>
2023-12-13 21:51:40 -0300

    chore: config jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  preset: 'ts-jest',
+  testMatch: ['**/**/*.spec.ts']
+
+}
